9092e1426834f5e3c8e0496dae53d82976c5d8c8
Marco Lara <19marcol95@gmail.com>
2025-07-31 19:57:44 -0700

    feat: create placeholder for QuestionAnsweringActivity

---
+++ app/src/main/java/com/group7/g7_trivia_game/QuestionAnsweringActivity.java
@@ -0,0 +1,26 @@
+package com.group7.g7_trivia_game.viewmodels;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.group7.g7_trivia_game.R;
+
+public class QuestionAnsweringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_question_answering);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

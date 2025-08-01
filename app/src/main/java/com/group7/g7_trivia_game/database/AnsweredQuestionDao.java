64d25d4057e0d196665a78ed1b8a59d1f3494642
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-07-31 19:20:01 -0700

    file structure stubs

---
+++ app/src/main/java/com/group7/g7_trivia_game/database/AnsweredQuestionDao.java
@@ -0,0 +1,10 @@
+package com.group7.g7_trivia_game.database;
+
+/**
+ * description
+ *
+ * @author
+ * @since 7/31/2025
+ */
+public class AnsweredQuestionDao {
+}
---
+++ app/src/main/java/com/group7/g7_trivia_game/database/entities/Question.java
@@ -0,0 +1,10 @@
+package com.group7.g7_trivia_game.database.entities;
+
+/**
+ * description
+ *
+ * @author
+ * @since 7/31/2025
+ */
+public class Question {
+}

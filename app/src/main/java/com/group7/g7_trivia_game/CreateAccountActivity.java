ba5586e461f2584d2a92fd7632ed71e66b75b110
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-02 16:47:31 -0700

    Added button listeners and return to login method.

--- app/src/main/java/com/group7/g7_trivia_game/CreateAccountActivity.java
+++ app/src/main/java/com/group7/g7_trivia_game/CreateAccountActivity.java
@@ -19,6 +19,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     private ActivityCreateAccountBinding binding;
     private CreateAccountActivityViewModel createAccountViewModel;
 
+    /**
+     * Sets binding and createAccountViewModel using context.
+     * Has button listeners for account creation and returning to login screen.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,17 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         createAccountViewModel = new ViewModelProvider(this).get(CreateAccountActivityViewModel.class);
 
+        //Button listeners for returning to the login screen or creating a new account
+        binding.createAccountBackButton.setOnClickListener(v -> {
+            returnToLogin();
+        });
 
+        binding.createAccountCreateAccountButton.setOnClickListener(v -> {
+            createAccount();
+        });
+    }
+
+    private void returnToLogin() {
+        startActivity(IntentFactory.loginActivityIntentFactory(getApplicationContext()));
     }
 }

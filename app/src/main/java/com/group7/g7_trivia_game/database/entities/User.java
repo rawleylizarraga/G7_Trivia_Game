4486cc2ae09c4bfb7c3f0cb319b2f1ffcf823b3e
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-02 14:47:25 -0700

    Added variables and constructor to User. Added Entity to User notation for Room.

--- app/src/main/java/com/group7/g7_trivia_game/database/entities/User.java
+++ app/src/main/java/com/group7/g7_trivia_game/database/entities/User.java
@@ -1,10 +1,29 @@
 package com.group7.g7_trivia_game.database.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.group7.g7_trivia_game.database.TriviaDatabase;
+
 /**
- * description
+ * User object for storing in database.
  *
- * @author
+ * @author Rawley Lizarraga
  * @since 7/31/2025
  */
+@Entity(tableName = TriviaDatabase.USER_TABLE)
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+    private int score;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = false;
+        this.score = 0;
+    }
 }

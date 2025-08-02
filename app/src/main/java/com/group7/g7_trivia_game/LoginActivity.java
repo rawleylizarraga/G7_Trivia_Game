df8f1eed81e318a95946563595bb779cf6b88a0b
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-02 16:21:01 -0700

    Added methods and buttons to LoginActivity for verifying username and password and starting MainActivity or AccountCreationActivity.

--- app/src/main/java/com/group7/g7_trivia_game/LoginActivity.java
+++ app/src/main/java/com/group7/g7_trivia_game/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.group7.g7_trivia_game;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +25,14 @@ public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
     private LoginActivityViewModel loginViewModel;
 
+    /**
+     * Updates binding and viewmodel with initial data.
+     * Contains button listeners for logging in and account creation.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +40,40 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         loginViewModel = new ViewModelProvider(this).get(LoginActivityViewModel.class);
+
+        //Button listeners to login or create account
+        binding.loginLoginButton.setOnClickListener(v -> {
+            verifyUser();
+        });
+
+        binding.loginCreateAccountButton.setOnClickListener(v -> {
+            createAccount();
+        });
+    }
+
+    /**
+     * Method to verify username and password.
+     * If both are successfully verified, start MainActivity and send the userId with the application context.
+     * If either fail, create a toast with an error message.
+     */
+    private void verifyUser() {
+        String username = binding.loginUsernameEditText.getText().toString().trim();
+        String password = binding.loginPasswordEditText.getText().toString().trim();
+
+        loginViewModel.getUserByUsername(username).observe(this, user -> {
+            if (user != null && user.getPassword().equals(password)) {
+                Toast.makeText(this, "Logging in...", Toast.LENGTH_SHORT).show();
+                startActivity(IntentFactory.mainActivityIntentFactory(getApplicationContext(), user.getId()));
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Method to start the account creation activity.
+     */
+    private void createAccount() {
+        startActivity(IntentFactory.createAccountActivityIntentFactory(getApplicationContext()));
     }
 }

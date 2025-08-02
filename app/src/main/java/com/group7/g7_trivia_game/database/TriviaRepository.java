4520a311564f3ac60b52ae22a2f2ffcd933e9077
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-02 14:17:01 -0700

    Populate repository
    Added DAO variables and constructor to TriviaRepository.

--- app/src/main/java/com/group7/g7_trivia_game/database/TriviaRepository.java
+++ app/src/main/java/com/group7/g7_trivia_game/database/TriviaRepository.java
@@ -1,5 +1,7 @@
 package com.group7.g7_trivia_game.database;
 
+import android.app.Application;
+
 /**
  * description
  *
@@ -7,4 +9,14 @@ package com.group7.g7_trivia_game.database;
  * @since 7/31/2025
  */
 public class TriviaRepository {
+    private UserDao mUserDao;
+    private QuestionDao mQuestionDao;
+    private AnsweredQuestionDao mAnsweredQuestionDao;
+
+    TriviaRepository(Application application){
+        TriviaDatabase db =TriviaDatabase.getDatabase(application);
+        mUserDao = db.userDao();
+        mQuestionDao = db.questionDao();
+        mAnsweredQuestionDao = db.answeredQuestionDao();
+    }
 }

eeaf738bec77cdf8af1641f9315866036a5255be
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-02 17:25:13 -0700

    Added method for creating a new User. Username and password are verified before User creation.

--- app/src/main/java/com/group7/g7_trivia_game/CreateAccountActivity.java
+++ app/src/main/java/com/group7/g7_trivia_game/CreateAccountActivity.java
@@ -1,10 +1,12 @@
 package com.group7.g7_trivia_game;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.group7.g7_trivia_game.database.entities.User;
 import com.group7.g7_trivia_game.databinding.ActivityCreateAccountBinding;
 import com.group7.g7_trivia_game.viewmodels.CreateAccountActivityViewModel;
 
@@ -45,7 +47,58 @@ public class CreateAccountActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Returns to the login screen by calling the intent factory for the login activity.
+     */
     private void returnToLogin() {
         startActivity(IntentFactory.loginActivityIntentFactory(getApplicationContext()));
+    }
+
+    /**
+     * Verifies that the entered username and password are valid.
+     * Then checks if the username already exists by using getUserByUsername and checking if result is void.
+     * If the username does not belong to an existing User, then a new User is created with the given username and password.
+     * After the new User is created, the MainActivity/Landing page is called by its intent factory with the new User's userId.
+     */
+    private void createAccount() {
+        String username = binding.createAccountUsernameEditText.getText().toString().trim();
+        String password = binding.createAccountPasswordEditText.getText().toString().trim();
+        String passwordConfirm = binding.createAccountConfirmEditText.getText().toString().trim();
+
+        //Verify that then input username and password are not empty and that the passwords match.
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter a username and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (username.length() > 12) {
+            Toast.makeText(this, "Username must me 10 characters or shorter", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(passwordConfirm)) {
+            Toast.makeText(this, "Please confirm the password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Use observers to check if the input username is already taken by seeing if getUserByUsername returns a User (already taken) or null (not taken)
+        createAccountViewModel.getUserByUsername(username).observe(this, user -> {
+            if (user != null) {
+                Toast.makeText(this, "Username is taken", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                Toast.makeText(this, "Creating account", Toast.LENGTH_SHORT).show();
+                User newUser = new User(username, password);
+                createAccountViewModel.insertUser(newUser);
+
+                //Uses an observer to ensure that the userId is successfully retrieved before starting the MainActivity/Landing page
+                createAccountViewModel.getUserByUsername(username).observe(this, userCheck -> {
+                    if (userCheck != null) {
+                        startActivity(IntentFactory.mainActivityIntentFactory(getApplicationContext(), userCheck.getId()));
+                    } else {
+                        Toast.makeText(this, "Failed to create account", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                });
+            }
+        });
     }
 }

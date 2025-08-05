73b57904ff5c3676934a093d124960d6fa3c1571
madisonnolen <madisonnolen03@gmail.com>
2025-08-05 11:25:10 -0700

    Initial LeaderboardViewModel
    This commit introduces the `LeaderboardViewModel` class.
    
    The `LeaderboardViewModel` extends `AndroidViewModel` and is responsible for providing data related to the leaderboard to the UI.
    
    Key functionalities include:
    - Initializing a `TriviaRepository` instance.
    - Exposing a `LiveData` object, `getTopUsers()`, which retrieves the top users sorted by score from the repository.

---
+++ app/src/main/java/com/group7/g7_trivia_game/viewmodels/LeaderboardViewModel.java
@@ -0,0 +1,24 @@
+package com.group7.g7_trivia_game.viewmodels;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.group7.g7_trivia_game.database.TriviaRepository;
+import com.group7.g7_trivia_game.database.entities.User;
+
+import java.util.List;
+
+public class LeaderboardViewModel extends AndroidViewModel {
+    private final TriviaRepository repository;
+
+    public LeaderboardViewModel(Application application) {
+        super(application);
+        repository = TriviaRepository.getRepository(application); // Method in repo
+    }
+
+    public LiveData<List<User>> getTopUsers() {
+        return repository.getTopUsersByScore(); // Method in repo
+    }
+}

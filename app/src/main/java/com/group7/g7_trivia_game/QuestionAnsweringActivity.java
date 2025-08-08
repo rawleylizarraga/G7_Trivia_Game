34c0f176b3d9f9006bfd5719ad769afeba9c83b2
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-07 19:19:56 -0700

    Remove unneeded line.

--- app/src/main/java/com/group7/g7_trivia_game/QuestionAnsweringActivity.java
+++ app/src/main/java/com/group7/g7_trivia_game/QuestionAnsweringActivity.java
@@ -32,7 +32,6 @@ public class QuestionAnsweringActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question_answering);
         binding = ActivityQuestionAnsweringBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 

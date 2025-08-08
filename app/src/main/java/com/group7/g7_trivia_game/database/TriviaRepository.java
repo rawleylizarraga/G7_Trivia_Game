807b77cf0d455ad455f8c9f176bdedc8a975a188
madisonnolen <madisonnolen03@gmail.com>
2025-08-07 17:42:08 -0700

    Refactor: Added method in TriviaRepository

--- app/src/main/java/com/group7/g7_trivia_game/database/TriviaRepository.java
+++ app/src/main/java/com/group7/g7_trivia_game/database/TriviaRepository.java
@@ -34,8 +34,6 @@ public class TriviaRepository {
         mAnsweredQuestionDao = db.answeredQuestionDao();
     }
 
-
-
     /**
      * Inserts a given {@code User} into the database by calling the DAO.
      * @param user {@code User} to add to the database.
@@ -114,30 +112,9 @@ public class TriviaRepository {
 
 
     //ADD ADDITIONAL METHODS AS NEEDED//
-
     public LiveData<List<User>> getTopUsersByScore() {
         return mUserDao.getTopUsersByScore();
     }
-
-    public LiveData<List<User>> getAllUsers() {
-        return mUserDao.getAllUsers();
-    }
-
-    // Returns one random question from a provided list of question IDs
-    public LiveData<Question> getRandomQuestionFromIds(List<Integer> ids) {
-        return mQuestionDao.getRandomQuestionFromIds(ids);
-    }
-
-    // Returns all question IDs from the question_table
-    public LiveData<List<Integer>> getAllQuestionIds() {
-        return mQuestionDao.getAllQuestionIds();
-    }
-
-    // Returns all answered question IDs from the answered_question_table
-    public LiveData<List<Integer>> getAllAnsweredQuestionIds() {
-        return mAnsweredQuestionDao.getAllAnsweredQuestionIds();
-    }
-
 
 
 }

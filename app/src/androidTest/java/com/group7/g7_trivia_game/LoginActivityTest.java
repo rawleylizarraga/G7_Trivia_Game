937eb0fd5170d7d8b7b8965a3bbf16eeec49c5ac
Rawley Lizarraga <rawleylizarraga@gmail.com>
2025-08-11 23:33:33 -0700

    Added comments.

--- app/src/androidTest/java/com/group7/g7_trivia_game/LoginActivityTest.java
+++ app/src/androidTest/java/com/group7/g7_trivia_game/LoginActivityTest.java
@@ -22,16 +22,19 @@ import org.junit.Test;
  */
 public class LoginActivityTest {
 
-    //deprecated but still works
+    //Deprecated but still works
     @Rule
     public IntentsTestRule<LoginActivity> intentsTestRule = new IntentsTestRule<>(LoginActivity.class);
 
     @Test
     public void createAccountButton() {
+        // Stubs the intent. Not sure if needed here but the android dev tutorial has it and it doesn't break anything so here it is.
         intending(hasComponent(CreateAccountActivity.class.getName())).respondWith(new Instrumentation.ActivityResult(0, null));
 
+        // Simulate pressing the create account button to start intent.
         onView(withId(R.id.loginCreateAccountButton)).perform(click());
 
+        // Check that it created the correct intent. (CreateAccountActivity)
         intended(hasComponent(CreateAccountActivity.class.getName()));
     }
 

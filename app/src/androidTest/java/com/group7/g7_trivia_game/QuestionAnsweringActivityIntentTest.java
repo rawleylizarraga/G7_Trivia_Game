299717fe37a44494fea90b8de5ab2c67af2918fd
Marco Lara <19marcol95@gmail.com>
2025-08-13 22:33:19 -0700

    Added test template file

---
+++ app/src/androidTest/java/com/group7/g7_trivia_game/QuestionAnsweringActivityIntentTest.java
@@ -0,0 +1,19 @@
+package com.group7.g7_trivia_game;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class QuestionAnsweringActivityIntentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
